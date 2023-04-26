5f09c97830b6d4d312b35b2756b63cccb154e406
SII <kubuntu@kubuntu2004.linuxvmimages.local>
2023-04-26 11:10:13 +0200

    4dof init

---
+++ exemples/4dof_arm/src/command/command/command.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+# See related drawio file to get the different distances and robot initial position
+
+import rclpy
+import rclpy.node
+
+from geometry_msgs.msg import Point
+from messages.msg import MotorsAngles
+from . import levenberg as lv
+
+import numpy as np
+import math
+from itertools import product
+
+# Here we define all the constants we have on the arm
+a1 = 100.0
+a2 = 100.0
+a3 = 100.0
+a4 = 100.0
+
+class Algo(rclpy.node.Node):
+    def __init__(self):
+        super().__init__('algoTwoDdl')
+        # create a publisher to put the values of motos angles
+        self.publisher = self.create_publisher(
+            MotorsAngles, 
+            'motors_angles', 
+            10) 
+        # create a subscription to get the Point target (x, y, z)
+        self.subscription = self.create_subscription(
+            Point,
+            'cmd_arm',
+            self._cmd_received_callback,
+            10)
+        self.subscription # prevent unused variable warning
+
+    def _cmd_received_callback(self, message):
+        """Handle new command message."""
+
+        x = message.x
+        y = message.y
+        z = message.z
+
+        print('received message')
+        print('Objectif :')
+        print(f'X = {x}') 
+        print(f'Y = {y}') 
+        print(f'Z = {z}')
+        print('')
+
+        pos = [x, y, z]
+        joints = [a1, a2, a3, a4]
+
+        # Target location using L-M with LLS estimation as starting point and lambda = 0.1
+        f, Df = lv.non_linear_least_squares_functions(pos, joints)
+        x_lm, history_lm = lv.levenberg_marquardt(f, Df, [0,0,0], 0.1, 1e-4)
+
+        thetas = np.degrees(x_lm) % 360
+        
+        # I have a problem on theta1 that can be > 180, AND MY SERVO CANNOT
+        # if thetas[0] > 180:
+        #     thetas[0] -= 180
+        #     thetas[1] = 180 - thetas[1]
+        
+
+        # Then, we publish to the motor command
+        motors_angles = MotorsAngles()
+        motors_angles.theta_1 = thetas[0]
+        motors_angles.theta_2 = thetas[1]
+        motors_angles.theta_3 = thetas[2]
+        motors_angles.theta_4 = thetas[3]
+        self.get_logger().info(f"theta_1 = {thetas[0]}, theta_2 = {thetas[1]}, theta_3 = {thetas[2]}, theta_4 = {thetas[3]} ", once=False)
+
+        self.publisher.publish(motors_angles)
+            
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    algo_4dof = Algo()
+
+    
+    rclpy.spin(algo_4dof)
+
+    algo_4dof.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+   main()
+
---
+++ exemples/4dof_arm/src/command/command/levenberg.py
@@ -0,0 +1,59 @@
+"""
+Date: 2023-03-27
+Author: Romain
+This script was developed for target positionning with different algorithms.
+"""
+
+import numpy as np
+
+# a0 = 0
+# a1 = 3.75
+# a2 = 3.25
+# a3 = 6.05
+# joints = [a0,a1,a2,a3]
+# x = 0.87
+# y = 0
+# z = 10
+# pos = [x,y,z] #x,y,z
+
+
+def non_linear_least_squares_functions(pos, joints):
+
+    # f(x) returns the residuals
+    f = lambda x: np.array([joints[1]*np.cos(x[0]) + joints[2]*np.cos(x[0]+x[1]) + joints[3]*np.cos(x[0]+x[1]+x[2]) - pos[0],
+                joints[0] + joints[1]*np.sin(x[0]) + joints[2]*np.sin(x[0]+x[1]) + joints[3]*np.sin(x[0]+x[1]+x[2]) - pos[2]])
+    # Df(x) is the N by 2 derivative matrix
+    Df = lambda x: np.array([[-joints[1]*np.sin(x[0]) - joints[2]*np.sin(x[0]+x[1]) - joints[3]*np.sin(x[0]+x[1]+x[2]), -joints[2]*np.sin(x[0]+x[1]) - joints[3]*np.sin(x[0]+x[1]+x[2]), -joints[3]*np.sin(x[0]+x[1]+x[2])],
+                             [joints[1]*np.cos(x[0]) + joints[2]*np.cos(x[0]+x[1]) + joints[3]*np.cos(x[0]+x[1]+x[2]), joints[2]*np.cos(x[0]+x[1]) + joints[3]*np.cos(x[0]+x[1]+x[2]), joints[3]*np.cos(x[0]+x[1]+x[2])]])
+    return f, Df
+
+def levenberg_marquardt(f, Df, x0, lambda0, tol = 1e-2, kmax = 100):
+    n = len(x0)
+    x = x0
+    lam = lambda0
+    obj = np.zeros((0,1))
+    res = np.zeros((0,1))
+    for k in range(kmax):
+        obj = np.vstack([obj, np.linalg.norm(f(x))**2])
+        res = np.vstack([res, np.linalg.norm(2*Df(x).T @ f(x))])
+        if np.linalg.norm(2*Df(x).T @ f(x)) < tol:
+            break
+        xt = x - np.linalg.inv((Df(x).T @ Df(x)) + lam*np.eye(n)) @ (Df(x).T @ f(x))
+        if np.linalg.norm(f(xt)) < np.linalg.norm(f(x)) :
+            lam = 0.8*lam
+            x = xt
+        else:
+            lam = 2.0*lam
+    return x, {'Objective':obj, 'Residual':res}
+
+
+
+if __name__ == '__main__':
+
+    # Target location using L-M with LLS estimation as starting point and lambda = 0.1
+    f, Df = non_linear_least_squares_functions(pos, joints)
+    x_lm, history_lm = levenberg_marquardt(f, Df, [0,0,0], 0.1, 1e-4)
+    #print(f(x_lls))
+    print(np.degrees(x_lm) % 360)
+
+    
